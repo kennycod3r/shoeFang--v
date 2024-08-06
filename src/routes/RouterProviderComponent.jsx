b226f8e5cab9ebeb4bafd5bbf34f73f337cac0e3
kennycod3r <oguntolakenny6@gmail.com>
2024-08-06 03:17:21 +0100

    com

--- src/routes/RouterProviderComponent.jsx
+++ src/routes/RouterProviderComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../Error";
-import ShoeFangTwo from "../pages/shoefangtwo";
+//import ShoeFangTwo from "../pages/shoefangtwo";
 import Journal from "../pages/journal";
 import Checkout from "../pages/checkout";
 import Sale from "../pages/sale";
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/shoefangtwo", element: <ShoeFangTwo /> },
+      //{ path: "/", element: <ShoeFangTwo /> },
       { path: "/journal", element: <Journal /> },
       { path: "/checkout", element: <Checkout /> },
       { path: "/sale", element: <Sale /> },
